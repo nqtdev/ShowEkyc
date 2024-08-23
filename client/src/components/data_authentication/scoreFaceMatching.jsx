a7b3a8858edfed81f724268ed0ac1fbf9896177d
nqtien <nguyenquoctien2000@gmail.com>
2024-08-23 14:57:06 +0700

    Update: Show data authentic
    Displays key information fields

---
+++ client/src/components/data_authentication/scoreFaceMatching.jsx
@@ -0,0 +1,29 @@
+import { useContext, useState, useEffect } from "react";
+import { WebSocketContext } from "../../config/SocketContext";
+
+const ScoreFaceMatching = () => {
+  const [scoreFaceMatching, setScoreFaceMatching] = useState();
+  const data = useContext(WebSocketContext);
+
+  useEffect(() => {
+    if (data?.cmdType == "SendBiometricAuthentication") {
+      const score = data.data.score;
+      setScoreFaceMatching(score);
+    } else {
+      setScoreFaceMatching(null);
+    }
+  }, [data]);
+  const scoreColor = scoreFaceMatching > 90 ? "text-green-600" : "text-red-600";
+  return (
+    <div className="text-left ">
+      <span>
+        Score when comparing fingerprints:{" "}
+        <span className={`text-2xl font-bold ${scoreColor}`}>
+          {scoreFaceMatching}
+        </span>
+      </span>
+    </div>
+  );
+};
+
+export default ScoreFaceMatching;
